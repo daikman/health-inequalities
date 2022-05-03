92fe5c4c9973045c2531c3bdf174ea3b1747a3cc
David Aikman <57766110+daikman@users.noreply.github.com>
2022-05-03 15:11:02 +0100

    add quiting smoking entry

--- R/config.R
+++ R/config.R
@@ -6,6 +6,30 @@ lineColours <- c(
 
 config <- list(
   list(
+    title = "Mental-illness related hospital stays",
+    dom_id = "mental-illness",
+    res_id = "ff797b7c-94cb-4efa-83dd-58e378cb7d8d",
+    SIMD = "SIMD",
+    metrics = c("PatientsRates" = "Patient rate", "DischargeRates" = "Discharge rate"),
+    explanations = c(
+      "Number of mental-illness patients per 100,000 population",
+      "Number of discharges per 100,000 population. A discarge is a release from hospital, including deaths."
+    ),
+    url = "https://www.opendata.nhs.scot/dataset/mental-health-inpatient-activity/resource/ff797b7c-94cb-4efa-83dd-58e378cb7d8d"
+  ),
+  list(
+    title = "Quiting Smoking",
+    dom_id = "quit-smoking",
+    res_id = "25e85a15-c1c4-4cef-9c87-b777beaa3323",
+    SIMD = "SIMDQuintile",
+    metrics = c("FourWeekQuitRate" = "Four week quit rate", "TwelveWeekQuitRate" = "Twelve week quit rate"),
+    explanations = c(
+      "Percentage of successful quits at 4-week follow-up",
+      "Percentage of successful quits at 12-week follow-up"
+    ),
+    url = "https://www.opendata.nhs.scot/dataset/nhs-smoking-cessation-service-statistics-scotland/resource/25e85a15-c1c4-4cef-9c87-b777beaa3323"
+  ),
+  list(
     title = "Suicide statistics",
     dom_id = "suicide",
     res_id = "f4e70964-38c9-44f6-aaba-08e38f8dbb2f",
@@ -34,22 +58,12 @@ config <- list(
     dom_id = "alcohol",
     res_id = "2d158224-1adf-4ffc-8e65-a05fe5712ea0",
     SIMD = "SIMDDecile",
-    metrics = c("AverageNumberOfStaysPerPatient" = "Avg. stays per patient"),
+    metrics = c("AverageNumberOfStaysPerPatient" = "Avg. stays per patient", "EASRPatients" = "Patient rate"),
     explanations = c(
-      "Number of hospital stays per patient, annually updated figure."
+      "Number of hospital stays per patient, annually updated figure.",
+      "The number of patients standardised by population size, age and sex."
     ),
     url = "https://www.opendata.nhs.scot/dataset/alcohol-related-hospital-statistics-scotland/resource/2d158224-1adf-4ffc-8e65-a05fe5712ea0"
-  ),
-  list(
-    title = "Mental illness related hospital stays",
-    dom_id = "mental-illness",
-    res_id = "ff797b7c-94cb-4efa-83dd-58e378cb7d8d",
-    SIMD = "SIMD",
-    metrics = c("PatientsRates" = "Patients per 100,000 pop."),
-    explanations = c(
-      "The annual number of patients with mental health problems relative to the size of the population in each deprivation group."
-    ),
-    url = "https://www.opendata.nhs.scot/dataset/mental-health-inpatient-activity/resource/ff797b7c-94cb-4efa-83dd-58e378cb7d8d"
   ),
   list(
     title = "Ambulance incidents",
